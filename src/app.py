2389fbcd799342a1448164ff1e9e35854983c8ae
Christoph <chris.schwaegerl@gmail.com>
2018-04-20 18:09:14 +0200

    working except parenthesis

--- src/app.py
+++ src/app.py
@@ -1,17 +1,24 @@
 import sqlparse
 import sql2ra
+import test_sql2ra
 
-sql = 'select distinct name from person, eats where person.name = eats.name and gender=\'f\''
-sql2 = 'select distinct A.name, B.name from Eats A, Eats B where A.pizza = B.pizza'
 
-stmt = sqlparse.parse(sql)[0]
-stmt2 = sqlparse.parse(sql2)[0]
+# sql = 'select distinct name from person, eats where person.name = eats.name and gender=\'f\''
+# sql2 = 'select distinct A.name, B.name from Eats A, Eats B where A.pizza = B.pizza'
+# sql3 = 'select distinct name from Eats'
+# sql4 = 'select distinct * from Eats'
 
-ra = sql2ra.translate(stmt)
-ra2 = sql2ra.translate(stmt2)
+# stmt = sqlparse.parse(sql)[0]
+# stmt2 = sqlparse.parse(sql2)[0]
+# stmt3 = sqlparse.parse(sql3)[0]
+# stmt4 = sqlparse.parse(sql4)[0]
 
-identifiers = stmt2[8]
+# ra = sql2ra.translate(stmt)
+# ra2 = sql2ra.translate(stmt2)
+# ra3 = sql2ra.translate(stmt3)
+# ra4 = sql2ra.translate(stmt4)
 
-print(sql)
-print(stmt)
-print(type(stmt))
+# print(ra)
+# print(ra2)
+# print(ra3)
+# print(ra4)
--- src/sql2ra.py
+++ src/sql2ra.py
@@ -1,4 +1,91 @@
 import sqlparse
 
-def translate(statement: sqlparse.sql.Statement):
-    return 'NOT IMPLEMENTED YET'
+def translate(stmt: sqlparse.sql.Statement):
+    tokens = cleanTokens(stmt.tokens)
+    return project(tokens)
+
+def project(tokens: sqlparse.sql.TokenList):
+    for i in range(len(tokens)):
+        if tokens[i].match(sqlparse.tokens.DML, ['SELECT']):
+            projectionToken = tokens[i + 1]
+            if projectionToken.ttype == sqlparse.tokens.Token.Wildcard:
+                return select(tokens)
+            else:
+                return '\project_{{{0}}} {1}'.format(projectionToken.value.replace(', ', ',').replace(',', ', '), select(tokens))
+
+        return ''
+
+def select(tokens: sqlparse.sql.TokenList):
+    tables = getTables(tokens)
+    if hasattr(tokens[-1], 'M_OPEN'):
+        conditions = getConditions(tokens[-1])
+        return '\select_{{{0}}} {1}'.format(conditions, tables)
+    else:
+        return tables
+
+def getConditions(token: sqlparse.sql.Token):
+    conditions = token.value.replace('where ', '').replace(' = ', '=').replace('=', ' = ').split(' and ')
+    if len(conditions) == 1:
+            return conditions[0]
+    elif len(conditions) > 1:
+        formattedConditions = []
+        for condition in conditions:
+            formattedConditions.append('({})'.format(condition))
+        return ' and '.join(formattedConditions)
+    else:
+        return ''
+
+def getTables(tokens: sqlparse.sql.TokenList):
+    joinedIdentifiers = None
+    for i in range(len(tokens)):
+        if (tokens[i].match(sqlparse.tokens.Token.Keyword, ['FROM'])):
+            joinedIdentifiers = getIdentifiers(tokens[i + 1])
+            return joinedIdentifiers
+
+    return 'invalid'
+
+def getIdentifiers(obj):
+    if type(obj) == sqlparse.sql.Identifier:
+        if len(obj.tokens) == 1:
+            return obj.value
+        elif len(obj.tokens) > 1:
+            return renameIdentifiers(obj)
+    elif type(obj) == sqlparse.sql.IdentifierList:
+        identifiers = [identifier for identifier in obj.tokens if type(identifier) == sqlparse.sql.Identifier]
+        renamedIdentifiers = []
+        for identifier in identifiers:
+            if len(identifier.tokens) == 1:
+                renamedIdentifiers.append(identifier.value)
+            else:
+                renamedIdentifiers.append('({})'.format(renameIdentifiers(identifier.tokens)))
+            
+        joinedIdentifiers = ''
+        for i in range(len(renamedIdentifiers)):
+            if i == 0:
+                joinedIdentifiers = '({} \\cross {})'.format(renamedIdentifiers[i], renamedIdentifiers[i+1])
+            elif i > 1:
+                joinedIdentifiers = '{} \\cross {}'.format(joinedIdentifiers, renamedIdentifiers[i])
+
+        return joinedIdentifiers
+    else:
+        return ''
+
+def renameIdentifiers(tokens: sqlparse.sql.TokenList):
+    cleanedTokens = removeWhitespaceTokens(tokens)
+    for i in range(len(cleanedTokens)):
+        if type(cleanedTokens[i]) == sqlparse.sql.Identifier:
+            return '\\rename_{{{0}: *}} {1}'.format(cleanedTokens[i].value, cleanedTokens[i-1].value)
+    
+    return 'invalid'
+
+def cleanTokens(tokens: sqlparse.sql.TokenList):
+    tokens = removeWhitespaceTokens(tokens)
+    tokens = removeDistinct(tokens)
+    return tokens
+
+def removeWhitespaceTokens(tokens: sqlparse.sql.TokenList):
+    return [token for token in tokens if not token.is_whitespace]
+
+def removeDistinct(tokens: sqlparse.sql.TokenList):
+    return [token for token in tokens if not token.normalized == 'DISTINCT']
+
