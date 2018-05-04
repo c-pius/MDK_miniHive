ee84a8752d3fee868ff31426553139bd43da7eff
christoph-rgb <chris.schwaegerl@gmail.com>
2018-05-04 13:02:32 +0200

    implemented rule1 break_up_selections

--- src/app.py
+++ src/app.py
@@ -9,14 +9,33 @@ import raopt
 dd = {}
 dd["Person"] = {"name": "string", "age": "integer", "gender": "string"}
 dd["Eats"] = {"name": "string", "pizza": "string"}
-stmt = "\project_{Person.name, Eats.pizza}\select_{Person.name = Eats.name}(Person \cross Eats);"
-ra = radb.parse.one_statement_from_string(stmt)
 
+ra = radb.parse.one_statement_from_string("\select_{Person.gender='f' and Person.age=16}(Person) \cross Eats;")
+new = raopt.rule_break_up_selections(ra)
+print(new)
 
+# ra1 = radb.parse.one_statement_from_string("\select_{Person.gender = 'f' and Person.age = 16}(Person);")
+# ra2 = radb.parse.one_statement_from_string("\project_{name}(\select_{gender='f' and age=16}(Person));")
+# ra3 = radb.parse.one_statement_from_string("\select_{Person.gender='f' and Person.age=16}(Person) \cross Eats;")
+# ra4 = radb.parse.one_statement_from_string("\select_{E.pizza = 'mushroom' and E.price < 10} \\rename_{E: *}(Eats);")
 
-ra1 = raopt.rule_break_up_selections(ra)
-print(ra)
-print(type(ra))
+# new1 = raopt.rule_break_up_selections(ra1)
+# new2 = raopt.rule_break_up_selections(ra2)
+# new3 = raopt.rule_break_up_selections(ra3)
+# new4 = raopt.rule_break_up_selections(ra4)
+
+# print(new1)
+# print(new2)
+# print(new3)
+# print(new4)
+
+#stmt = "\project_{Person.name, Eats.pizza}\select_{Person.name = Eats.name}(Person \cross Eats);"
+#stmt = "\project_{Person.name, Eats.pizza}(\select_{Person.gender = 'f' and Person.age = 16}(Person \cross Eats));"
+#ra = radb.parse.one_statement_from_string(stmt)
+
+#ra1 = raopt.rule_break_up_selections(ra)
+#print(ra1)
+#print(type(ra1))
 #ra2 = raopt.rule_push_down_selections(ra1, dd)
 #ra3 = raopt.rule_merge_selections(ra2)
 #ra4 = raopt.rule_introduce_joins(ra3)
--- src/raopt.py
+++ src/raopt.py
@@ -1,2 +1,29 @@
-def rule_break_up_selections(ra):
-    return ra
+import radb.ast
+
+def rule_break_up_selections(stmt):
+    #break up a given select statement
+    if type(stmt) == radb.ast.Select:
+        return break_up_selections_internal(stmt)
+    #create a new select statement with the broken up select statement
+    elif type(stmt) == radb.ast.Project:
+        return radb.ast.Project(stmt.attrs, break_up_selections_internal(stmt.inputs[0]))
+    #create a new cross from both sides broken up
+    elif type(stmt) == radb.ast.Cross:
+        left = rule_break_up_selections(stmt.inputs[0])
+        right = rule_break_up_selections(stmt.inputs[1])
+        return radb.ast.Cross(left, right)
+    #can't break up anything else
+    else:
+        return stmt   
+
+def break_up_selections_internal(selectStmt: radb.ast.Select):
+    #assumes that there is only one input
+    return break_up(selectStmt.cond.inputs, selectStmt.inputs[0])
+
+def break_up(conditions: [], input: radb.ast.RelExpr):
+    if len(conditions) == 1:
+        return radb.ast.Select(conditions[0], input)
+    else:
+        return radb.ast.Select(conditions[0], break_up(conditions[1:], input))
+
+
